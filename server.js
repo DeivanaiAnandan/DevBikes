75027747ae5de9f15eff9bb188bb59be4d793cdb
DeivanaiAnandan <deivanai.anandan@gmail.com>
2024-02-15 14:51:14 +0530

    preinstallation

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.use(cors({
 app.use('/api/bikes/' , require('./routes/bikesRoute'));
 app.use('/api/users/' , require('./routes/usersRoute'));
 app.use('/api/bookings/' , require('./routes/bookingsRoute'));
+const path = require('path')
+
+if(process.env.NODE_ENV==='production')
+{
+
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+    })
+
+}
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 dbConnection();
 app.listen(port, () => console.log(`Node JS server started on port ${port}!`));
