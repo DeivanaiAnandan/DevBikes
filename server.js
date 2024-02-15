cce3659c8e0e484fe11f6166e0891d68a972fc1f
DeivanaiAnandan <deivanai.anandan@gmail.com>
2024-02-15 15:08:48 +0530

    ccc

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnection = require('./db')
+const path = require('path')
+
 
 app.use(express.json())
 app.use(cors({
@@ -11,21 +13,13 @@ app.use(cors({
 app.use('/api/bikes/' , require('./routes/bikesRoute'));
 app.use('/api/users/' , require('./routes/usersRoute'));
 app.use('/api/bookings/' , require('./routes/bookingsRoute'));
-const path = require('path')
 
+var __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/build"))); 
+app.get("*", (req, res) =>{
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+})
 
-    app.use('/' , express.static('client/build/public'))
-
-    app.get('*' , (req , res)=>{
-
-          res.sendFile(path.resolve(__dirname, 'client/build/public/index.html'));
-
-    })
-
-
-
-
-app.get('/', (req, res) => res.send('Hello World!'));
 dbConnection();
 app.listen(port, () => console.log(`Node JS server started on port ${port}!`));
 
